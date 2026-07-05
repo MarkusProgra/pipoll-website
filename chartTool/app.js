98d98bc01b5c88d739dced08e559db62f56652e7
Kusa <gua.mkhoffmann@gmail.com>
2026-07-04 18:13:38 -0600

    Change header option in sheet_to_json to 1

--- chartTool/app.js
+++ chartTool/app.js
@@ -48,7 +48,7 @@ fileInput.addEventListener('change', (e) => {
       const data = new Uint8Array(event.target.result);
       const workbook = XLSX.read(data, { type: 'array' });
       const sheet = workbook.Sheets[workbook.SheetNames[0]];
-      const jsonData = XLSX.utils.sheet_to_json(sheet, { header: 0 });
+      const jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
 
       console.log('Parsed data:', jsonData);
 
